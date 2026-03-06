2df225f52d063ed8c36e0531abc16d9566b8368b
Fenix <fenixxx1990@yandex.ru>
2026-03-06 19:19:17 +0400

    review

--- 10-weather-typescript-1/weatherRouter/weatherRouter.ts
+++ 10-weather-typescript-1/weatherRouter/weatherRouter.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { getIcon, getWeather } from "../services/api.service.js";
 import { printWeather, printError } from "../services/log.service.js";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const weatherRouter = express.Router();
 
@@ -14,17 +14,21 @@ weatherRouter.get("/:cityName", async (req, res) => {
   try {
     const weather = await getWeather(cityName);
     res.send(printWeather(weather, getIcon(weather.weather[0].icon)));
-  } catch (e) {
+  } catch (e: unknown) {
     if (axios.isAxiosError(e)) {
-      if (e?.response?.status === 404) {
-        res.send(printError("Неверно указан город"));
-      } else if (e?.response?.status === 401) {
-        res.send(printError("Неверно указан токен"));
+      const error = e as AxiosError;
+
+      if (error.response?.status === 404) {
+        printError("Неверно указан город");
+      } else if (error.response?.status === 401) {
+        printError("Неверно указан токен");
+      } else {
+        printError(error.message || "Произошла ошибка");
       }
+    } else if (e instanceof Error) {
+      printError(e.message);
     } else {
-      if (e instanceof Error) {
-        res.send(printError(e.message));
-      }
+      printError("Произошла неизвестная ошибка");
     }
   }
 });
